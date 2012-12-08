2499aaf721053d64e648431c1b4518fceee68772
James Fuller <jimminyfuller@gmail.com>
2012-12-07 23:22:35 -0500

    Add Apache License

--- lib/functions.php
+++ lib/functions.php
@@ -1,4 +1,17 @@
 <?php
+// Copyright 2011 James Fuller <jimminyfuller@gmail.com>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #Construct URL for making requests
 function build_url($path, $type, $data){
   $URI = '';
@@ -56,7 +69,7 @@ function image_create($file_path, $path, $photos, $keys){
     $ourFileHandle = fopen($url, 'w+') or die("can't open file");
     fclose($ourFileHandle);
 
-    imagejpeg($image, $url);
+    imagejpeg($image, $url, 100);
     imagedestroy($image);
     $url = $path.'tmp/'.$segment.'.jpg';
 
